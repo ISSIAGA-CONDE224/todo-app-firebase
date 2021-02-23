5fb057e42e3261736b233961e75a0d17fc01a6f7
ISSIAGA-CONDE224 <condeissiaga259@gmail.com>
2021-02-23 16:48:46 +0000

    J'ai enleve le widget gesturedetector

--- lib/widget/todo_widget.dart
+++ lib/widget/todo_widget.dart
@@ -95,65 +95,62 @@ class TodoWidget extends StatelessWidget {
         ),
       );
 
-  Widget buildTodo(BuildContext context) => GestureDetector(
-        onTap: () => editTodo(context, todo),
-        child: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(20),
-          child: Row(
-            children: [
-              Checkbox(
-                activeColor: Theme.of(context).primaryColor,
-                checkColor: Colors.white,
-                value: todo.isDone,
-                onChanged: (_) {
-                  final provider =
-                      Provider.of<TodosProvider>(context, listen: false);
-                  final isDone = provider.toggleTodoStatus(todo);
+  Widget buildTodo(BuildContext context) => Container(
+    color: Colors.white,
+    padding: EdgeInsets.all(20),
+    child: Row(
+      children: [
+        Checkbox(
+          activeColor: Theme.of(context).primaryColor,
+          checkColor: Colors.white,
+          value: todo.isDone,
+          onChanged: (_) {
+            final provider =
+                Provider.of<TodosProvider>(context, listen: false);
+            final isDone = provider.toggleTodoStatus(todo);
 
-                  Utils.showSnackBar(
-                    context,
-                    isDone
-                        ? 'Tâches terminée avec succes'
-                        : 'Tâche non terminée',
-                  );
-                },
-              ),
-              const SizedBox(width: 20),
-              Expanded(
-                child: Column(
-                  children: [
-                    !todo.isDone
-                        ? Text(
-                            todo.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                                fontSize: 22),
-                          )
-                        : Text(
-                            todo.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey,
-                                decoration: TextDecoration.lineThrough,
-                                fontSize: 22),
-                          ),
-                    if (todo.description.isNotEmpty)
-                      Container(
-                        margin: EdgeInsets.only(top: 4),
-                        child: Text(
-                          todo.description,
-                          style: TextStyle(fontSize: 20, height: 1.5),
-                        ),
-                      )
-                  ],
-                ),
-              ),
+            Utils.showSnackBar(
+              context,
+              isDone
+                  ? 'Tâches terminée avec succes'
+                  : 'Tâche non terminée',
+            );
+          },
+        ),
+        const SizedBox(width: 20),
+        Expanded(
+          child: Column(
+            children: [
+              !todo.isDone
+                  ? Text(
+                      todo.title,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 22),
+                    )
+                  : Text(
+                      todo.title,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontSize: 22),
+                    ),
+              if (todo.description.isNotEmpty)
+                Container(
+                  margin: EdgeInsets.only(top: 4),
+                  child: Text(
+                    todo.description,
+                    style: TextStyle(fontSize: 20, height: 1.5),
+                  ),
+                )
             ],
           ),
         ),
-      );
+      ],
+    ),
+  );
 
   void deleteTodo(BuildContext context, Todo todo) {
     final provider = Provider.of<TodosProvider>(context, listen: false);
